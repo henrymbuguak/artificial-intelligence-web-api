0b3d15c155b5c6dadb9473b1cd37713907b09d35
henrymbuguak <henrymbuguak@gmail.com>
2022-04-28 23:40:29 +0300

    [AI] Initial project setup

---
+++ src/app.py
@@ -0,0 +1,9 @@
+from crypt import methods
+from flask import Flask, redirect, url_for, render_template, session
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
